2b99bb8739bf0381c90a1758534be07c2949972b
Chris Jennison <cjennison@hitpointstudios.com>
2013-05-07 21:27:11 -0400

    Search Working for real this time

--- trunk/scripts/search.js
+++ trunk/scripts/search.js
@@ -1,6 +1,7 @@
 var queryData = [];
 var imageQueryData = [];
 var i = 0;
+var k = 0;
 
 var queryName;
 
@@ -8,13 +9,13 @@ var queryName;
 //Populate results from search
 function init(){
 	
-	console.log(complex);
+	//console.log(complex);
 	var getQuery = setInterval(function(){
 		if(complex != null){
 			startSearchQuery();
 			clearInterval(getQuery);
 		}
-	},500)
+	},2500)
 	
 	
 	//startSearchQuery();
@@ -23,11 +24,11 @@ function init(){
 
 function startSearchQuery(){
 	for(var i=0;i<complex.length;i++){
-		console.log(complex[i].primary);
+		//console.log(complex[i].primary);
 		complex[i].searchTermArray = complex[i].secondary.split(/,/);
 		//console.log(complex[i].searchTermArray)
 		complex[i].searchTermArray.push(complex[i].primary);
-		console.log(complex[i].searchTermArray)
+		//console.log(complex[i].searchTermArray)
 	}
 	
 	
@@ -40,7 +41,7 @@ function startSearchQuery(){
 	
 	for(var q = 0;q < complex.length;q++){
 		for(var searchIndex = 0; searchIndex < complex[q].searchTermArray.length ;searchIndex++){
-			console.log(complex[q].searchTermArray[searchIndex]);
+			//console.log(complex[q].searchTermArray[searchIndex]);
 			if(complex[q].searchTermArray[searchIndex] == queryName){
 				imageQueryData.push(complex[q]);
 			}
@@ -62,24 +63,64 @@ function startSearchQuery(){
 
 
 function displayQuery(){
-	if(i > imageQueryData.length - 1){ return; };
+	
+	
+	
+	
+	if(i > imageQueryData.length - 1){ 
+		//InitPictures(); 
+		return; 
+	};
+	
 	
 	var list = $("#results-list");
-	var listItem = $("<a href='image.php?photoid=" + imageQueryData[i].id + "&dest=" + $("#searchname").html() + "'><li><div class='search-img'>" +
+	var listItem = $("<a href='image.php?photoid=" + imageQueryData[i].id + "&dest=" + $("#searchname").html() + "'><li>" + "<div class='search-img'>" +
 	"<span class='photo-text'><center>" + imageQueryData[i].name + "</center></span>" + "</div></li></a>")
+	var searchImg = $(listItem).find(".search-img");
+	console.log(searchImg);
 	$(list).append(listItem);
 	
-	var imgArray = $(".search-img");
-	console.log(imgArray[i]);
-	$(imgArray[i]).attr("photo-id", imageQueryData[i].id);
-	$(imgArray[i]).css("background", "url(photos/" + imageQueryData[i].id + ".jpg)");
-	$(imgArray[i]).css("background-size", "100% 100%");
+	
+	$(searchImg).attr("photo-id", imageQueryData[i].id);
+	$(searchImg).css("background", "url(photos/" + imageQueryData[i].id + ".jpg)");
+	$(searchImg).css("background-size", "100% 100%");
+	
 	
 	i++;
-	
 	setTimeout(function(){
 		$(".search-img").css("opacity", 1)
 		displayQuery();
+	},100)
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+function InitPictures(){
+	console.log($(".search-img"))
+	if(k > imageQueryData.length - 1){  return; };
+	
+	
+	
+	var imgArray = $(".search-img");
+	//console.log($(".search-img")[k])
+	$(imgArray[k]).attr("photo-id", imageQueryData[k].id);
+	$(imgArray[k]).css("background", "url(photos/" + imageQueryData[k].id + ".jpg)");
+	$(imgArray[k]).css("background-size", "100% 100%");
+			
+	
+	//console.log("OK")
+	k++;
+	setTimeout(function(){
+		$(".search-img").css("opacity", 1)
+		
+		InitPictures();
+		
 	},100)
 	
 }
